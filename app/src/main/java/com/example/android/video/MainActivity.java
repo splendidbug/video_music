dbb89d7264cac9e50bedd6f0f4ed44f48a2a8c39
shreyas <shreyasanother@gmail.com>
2019-08-08 11:20:42 +0530

    Final

--- app/src/main/java/com/example/android/video/MainActivity.java
+++ app/src/main/java/com/example/android/video/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.video;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +16,7 @@ import android.widget.VideoView;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCompletionListener{
+public class MainActivity extends AppCompatActivity {
     Button playVid, playMusic, pauseMusic;
     VideoView videoView;
     MediaController mediaController;
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
     AudioManager audioManager; //for everything in android we have an object
     SeekBar seekBar2;
     Timer timer;
+    Handler handler;
+    Runnable runnable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +43,8 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
         //MediaController to know that we want to use this object inside main activity
 
         mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.znmd);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
 
         audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);// as audio manager is a service and the return type is an object
 
@@ -49,8 +54,9 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
         seekBar.setMax(maxVolume);
         seekBar.setProgress(currentVolume);
 
-        seekBar2.setMax(mediaPlayer.getDuration());
-        mediaPlayer.setOnCompletionListener(this);
+
+
+       // mediaPlayer.setOnCompletionListener(this);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -65,6 +71,15 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
+            }
+        });
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                seekBar2.setMax(mediaPlayer.getDuration());
+                playCycle();
+                mediaPlayer.start();
             }
         });
 
@@ -102,18 +117,18 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
             @Override
             public void onClick(View v) {
                 mediaPlayer.start();
-                timer = new Timer(); // we use timer object to update the ui component or thread.
+                //  timer = new Timer(); // we use timer object to update the ui component or thread.
                 // We created a new thread because the following task is heavy,
                 // we don't want to over load the main UI, hence we create a new thread,
                 // where the following task gets performed, not overloading the main UI
 
-                timer.scheduleAtFixedRate(new TimerTask() { // here we have a timer object that
+         /*       timer.scheduleAtFixedRate(new TimerTask() { // here we have a timer object that
                     // we want to schedule at a fixed rate a new timer task
                     @Override
                     public void run() {
                     seekBar2.setProgress(mediaPlayer.getCurrentPosition());
                     }
-                },0, 1000); // here we want the task to get updated over the period
+                },0, 1000);*/ // here we want the task to get updated over the period
             }
         });
 
@@ -125,12 +140,27 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
         });
 
     }
+    public void playCycle(){
+        seekBar2.setProgress(mediaPlayer.getCurrentPosition());
+        if(mediaPlayer.isPlaying()){
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                playCycle();
+                }
+            };
+            handler.postDelayed(runnable, 1000);
+        }
+    }
 
 
-    @Override
+}
+
+
+   /* @Override
     public void onCompletion(MediaPlayer mp) {
 
         timer.cancel(); // cancels the thread.
 
-    }
-}
+    }*/
+
